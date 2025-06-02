41960702f339efa2f6bf4fa0e3afef90476645e6
juliofigueiredo21 <juliofigueiredo21@gmail.com>
2025-06-02 09:02:18 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)", // Aplica para todas as rotas
         headers: [
           {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
-          }
-        ]
-      }
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
     ];
   },
 };
