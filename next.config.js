1bbffb6b54856e1b9dbc092b8a0a229385030fef
juliofigueiredo21 <juliofigueiredo21@gmail.com>
2025-06-02 07:36:28 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          }
+        ]
+      }
+    ];
+  },
+};
+
+module.exports = nextConfig;
