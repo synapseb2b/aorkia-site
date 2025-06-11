17bd6d5c7145ec5ae17cba2b1d0a9c2dde97d43e
juliofigueiredo21 <juliofigueiredo21@gmail.com>
2025-06-11 14:11:15 -0300

    Update contato.js

--- pages/contato.js
+++ pages/contato.js
@@ -36,46 +36,7 @@ export default function Contato() {
       </Head>
 
       <main className="bg-black text-white">
-        {/* Seção Hero - Estilo Jam3 com vídeo */}
-        <section className="relative h-screen overflow-hidden hero flex items-center justify-center">
-          <video autoPlay muted loop playsInline className="absolute inset-0 w-full h-full object-cover">
-            <source src="/image/video_hero.mp4" type="video/mp4" />
-            Seu navegador não suporta vídeo.
-          </video>
-          <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-black/50"></div>
-
-          <div className="container mx-auto max-w-6xl px-4 relative z-10">
-            <div className="flex flex-col items-center md:items-start text-center md:text-left">
-              <p className="text-lg sm:text-xl md:text-2xl text-gray-300 mb-4">
-                Entre em contato conosco.
-              </p>
-              <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight mb-8 tracking-tight text-white">
-                Fale com a <span className="text-primary">AORKIA</span>
-              </h1>
-              <p className="text-xl md:text-2xl max-w-3xl mb-12 text-gray-300">
-                Descubra como nossas soluções estratégicas podem impulsionar sua empresa.
-              </p>
-              
-              {/* Botão para rolar para o footer global */}
-              <button 
-                onClick={scrollToContact}
-                className="text-lg font-medium px-8 py-3 border text-white border-white hover:bg-white hover:text-black transition-all duration-500"
-              >
-                Contato
-              </button>
-            </div>
-          </div>
-
-          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 md:left-auto md:right-10 md:translate-x-0 flex justify-center animate-bounce">
-            <a 
-              href="#footer" 
-              onClick={scrollToContact}
-              className="text-white text-4xl"
-            >
-              <i className="ri-arrow-down-line"></i>
-            </a>
-          </div>
-        </section>
+        {/* A Hero section foi removida. A página agora terá apenas Navbar e Footer. */}
       </main>
     </>
   );
