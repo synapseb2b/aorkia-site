32c034f3257476205aae27ae9351e05a8840d1d5
juliofigueiredo21 <juliofigueiredo21@gmail.com>
2025-06-24 11:14:55 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -505,19 +505,19 @@ function MyApp({ Component, pageProps }) {
         <input
           type="checkbox"
           name="solucoes"
-          value="Operações de Bordas Inteligentes"
+          value="Visibilidade Estratégica de Dados Sensíveis"
           className="mr-3 w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary focus:ring-2"
         />
-        <span className="text-gray-800">Operações de Bordas Inteligentes</span>
+        <span className="text-gray-800">Segurança para Operações Críticas</span>
       </label>
       <label className="flex items-center">
         <input
           type="checkbox"
           name="solucoes"
-          value="Visibilidade Estratégica de Dados Sensíveis"
+          value="Operações de Bordas Inteligentes"
           className="mr-3 w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary focus:ring-2"
         />
-        <span className="text-gray-800">Segurança para Operações Críticas</span>
+        <span className="text-gray-800">Operações de Bordas Inteligentes</span>
       </label>
     </div>
   </div>
