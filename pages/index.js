e3c18d628c0de582ccdd15edffdb79f2d0c0c657
juliofigueiredo21 <juliofigueiredo21@gmail.com>
2025-06-24 20:30:36 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -116,8 +116,8 @@ const products = [
                 Boas-vindas à AORKIA.
               </p>
               <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight mb-8 tracking-tight">
-                Tecnologia de Ponta ativada com propósito.  <br className="hidden md:block" />
-                Inteligência entregue com precisão.
+                Onde a tecnologia de ponta se transforma <br className="hidden md:block" />
+                em resultados reais e mensuráveis.
               </h1>
             </div>
           </div>
