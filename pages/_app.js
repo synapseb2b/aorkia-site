8ab357341b4c2a6edd468d59713b99da6cc7da2d
juliofigueiredo21 <juliofigueiredo21@gmail.com>
2025-06-25 01:18:38 -0300

    Atualizar o _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -512,7 +512,7 @@ function MyApp({ Component, pageProps }) {
           value="Governança Estratégica de Dados Sensíveis"
           className="mr-3 w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary focus:ring-2"
         />
-        <span className="text-gray-800">Visibilidade Estratégica de Dados Sensíveis</span>
+        <span className="text-gray-800">Governança Estratégica de Dados Sensíveis</span>
       </label>
       <label className="flex items-center">
         <input
@@ -689,3 +689,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+ 
