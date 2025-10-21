bcd90a3bed7ae2c8b41d7b7490f1f2072ab56a2d
Júlio Figueiredo <dev@synapseb2b.com>
2025-10-21 19:12:22 -0300

    Update backup_saas_estrategico.js

--- pages/backup_saas_estrategico.js
+++ pages/backup_saas_estrategico.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ShieldAlert, UserX, FileSearch } from 'lucide-react';
-
+ 
 const faqData = [
   {
     question: "O que é Keepit?",
