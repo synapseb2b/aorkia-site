85212a9276574ff426caa8b1026f84b7bf7986d2
Júlio Figueiredo <dev@synapseb2b.com>
2025-10-21 06:04:39 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -224,9 +224,9 @@ function MyApp({ Component, pageProps }) {
           <div className="preloader-content text-center">
            <div className="logo-container h-20 relative overflow-hidden flex items-center">
             <Image
-              src="/logo/logo_symbol_aorkia.png"
+              src="/logo/logo_aorkia.png"
               alt="AORKIA"
-              className="h-20 w-auto"
+              className="h-24 w-auto"
               width={200}
               height={80}
               quality={100}
@@ -245,9 +245,9 @@ function MyApp({ Component, pageProps }) {
           <Link href="/" className="flex-shrink-0 transition-transform duration-300 hover:scale-105">
             <div className="logo-container h-20 relative">
               <Image
-                src="/logo/logo_symbol_aorkia.png"
+                src="/logo/logo_aorkia.png"
                 alt="AORKIA"
-                className="h-20 w-auto"
+                className="h-24 w-auto"
                 width={200}
                 height={80}
                 quality={100}
@@ -343,9 +343,9 @@ function MyApp({ Component, pageProps }) {
           <div className="py-2">
             <div className="logo-container h-16 relative flex items-center">
               <Image
-                src="/logo/logo_symbol_aorkia.png"
+                src="/logo/logo_aorkia.png"
                 alt="AORKIA"
-                className="h-16 w-auto"
+                className="h-24 w-auto"
                 width={200}
                 height={80}
                 priority
@@ -374,9 +374,9 @@ function MyApp({ Component, pageProps }) {
               <div className="flex justify-between items-center p-6 border-b border-white/10">
                 <div className="logo-container h-12 relative">
                   <Image
-                    src="/logo/logo_symbol_aorkia.png"
+                    src="/logo/logo_aorkia.png"
                     alt="AORKIA"
-                    className="h-12 w-auto"
+                    className="h-24 w-auto"
                     width={200}
                     height={80}
                     priority
@@ -517,9 +517,9 @@ function MyApp({ Component, pageProps }) {
             <div className="order-2 lg:order-1 bg-white/10 backdrop-blur-sm p-8 rounded-lg shadow-inner">
               <div className="mb-12">
                 <Image
-                  src="/logo/logo_symbol_aorkia.png" // Ajuste o caminho se necessário
+                  src="/logo/logo_aorkia.png" // Ajuste o caminho se necessário
                   alt="Synapse B2B"
-                  className="h-20 w-auto mb-6"
+                  className="h-24 w-auto mb-6"
                   width={416}
                   height={166}
                   quality={100}
